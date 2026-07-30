d89dd0995948db5bf79f8b75a89827152bbbaa29
Mana-studi <studi829@gmail.com>
2026-07-30 14:27:29 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//apesan untuk anda 😋//
+const tombol = document.getElementById("pesanRahasia");
+const teks = document.getElementById("Hasil");
+ //fungsi aja sih //
+tombol.addEventListener("click",function() {
+    teks.innerText = "hebatt anda kamu kalian tombol bekerja"
+    tombol.style.backgroundColor = "lightgreen";
+});
